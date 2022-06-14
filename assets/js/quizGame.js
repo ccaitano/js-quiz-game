74052065aca8fc06ff45533b935369b5855abdcd
ccaitano <cheryl.caitano@gmail.com>
2022-06-13 21:36:48 -1000

    Added basic HTML styling to main quiz page

---
+++ assets/js/quizGame.js
@@ -0,0 +1,14 @@
+//Declare Countdown (numbers)
+//Declare Questions (array)
+var questions = [
+    {
+        //question
+        //answers
+        //correct answer
+    },
+    {
+        //question
+        //answers
+        //correct answer
+    }
+];
