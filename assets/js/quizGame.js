f6137168ebff3eb0cb6f70f408636312e5a81378
ccaitano <cheryl.caitano@gmail.com>
2022-06-13 22:15:11 -1000

    Added time function into js file

--- assets/js/quizGame.js
+++ assets/js/quizGame.js
@@ -1,14 +1,31 @@
 //Declare Countdown (numbers)
 //Declare Questions (array)
-var questions = [
-    {
-        //question
-        //answers
-        //correct answer
-    },
-    {
-        //question
-        //answers
-        //correct answer
-    }
-];
+// var questions = [
+//     {
+//         //question
+//         //answers
+//         //correct answer
+//     },
+//     {
+//         //question
+//         //answers
+//         //correct answer
+//     }
+// ];
+
+var timeEl = document.querySelector(".timer");
+var countdownEl = document.getElementById("countdown");
+
+var secondsLeft = 30;
+
+function setTime () {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        countdownEl.textContent = secondsLeft;
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+}
+
+setTime();
