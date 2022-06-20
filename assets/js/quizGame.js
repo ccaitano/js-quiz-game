85b6596adf8423a32ed5902bba36b7fd1ca97ed6
ccaitano <cheryl.caitano@gmail.com>
2022-06-19 21:18:17 -1000

    Adding in question array and answer button styling

--- assets/js/quizGame.js
+++ assets/js/quizGame.js
@@ -1,10 +1,12 @@
 //Declare Countdown (numbers)
 var timeEl = document.querySelector(".timer");
-var countdownEl = document.getElementById("countdown");
 var startQuizEl = document.querySelector("#startButton");
-var secondsLeft = 30;
+var highScoreLink = document.querySelector("#HSLink");
+var secondsLeft = 60;
 
+//Timer Function
 function setTime () {
+    var countdownEl = document.getElementById("countdown");
     var timerInterval = setInterval(function() {
         secondsLeft--;
         countdownEl.textContent = secondsLeft;
@@ -14,9 +16,30 @@ function setTime () {
     }, 1000);
 }
 
+// Declare Questions (array)
+var quizArray = [
+    {
+        id: 1,
+        question: "From the given array which index is the letter 'b' on? ['a', 'b', 'c', 'd']?",
+        answers: ["0", "1", "2","3"],
+        correctAnswer: "0"
+    },
+    {
+        id: 2,
+        question: "What are the two types of scope JavaScript uses?",
+        answers: ["Inner and Outer", "Main and Peripheral", "Global and Local", "Inclusive and Exclusive"],
+        correctAnswer: "Global and Local"
+    }
+];
+
 function startQuiz () {
-    startQuizEl.style.display = 'none';
+    //Declare variables
     var qandaScreenEl = document.getElementById("mainScreen");
+
+    //Remove 'Begin Quiz' Button
+    startQuizEl.style.display = 'none';
+    highScoreLink.style.display = 'none';
+    //Create quiz screen display - Question, Answers and Timer
     qandaScreenEl.setAttribute('id', 'mainScreen2');
     qandaScreenEl.innerHTML =
     `<div class="grid-question-answer">
@@ -26,10 +49,10 @@ function startQuiz () {
         </div>
         <div class="answers">
             <h2>//Answers</h2>
-            <button type="button" class="displayAnswers" id="answer1"></button>
-            <button type="button" class="displayAnswers" id="answer2"></button>
-            <button type="button" class="displayAnswers" id="answer3"></button>
-            <button type="button" class="displayAnswers" id="answer4"></button>
+            <button class="displayAnswers" id="answer1"></button>
+            <button class="displayAnswers" id="answer2"></button>
+            <button class="displayAnswers" id="answer3"></button>
+            <button class="displayAnswers" id="answer4"></button>
         </div>
     </div>   
     <div class="timer">
@@ -37,28 +60,33 @@ function startQuiz () {
         <p id="countdown"></p>
     </div>`
     ;  
+    //Start Timer
+    setTime();
+
+    var questionEl = document.getElementById("displayQuestion");
+    var answer1El = document.getElementById("answer1");
+    var answer2El = document.getElementById("answer2");
+    var answer3El = document.getElementById("answer3");
+    var answer4El = document.getElementById("answer4");
+    questionEl.textContent = quizArray[1].question;
+    answer1El.textContent = "A. " + quizArray[1].answers[0];
+    answer2El.textContent = "B. " + quizArray[1].answers[1];
+    answer3El.textContent = "C. " + quizArray[1].answers[2];
+    answer4El.textContent = "D. " + quizArray[1].answers[3];
+
+    // var nextQuestion = document.createElement("button");
+    // nextQuestion.textContent = "Next Question";
+    // nextQuestion.setAttribute("style", "display: flex; justify-content: center; font-size: 25px; font-family: courier; font-weight: bold; color: #1E1E1E; background-color: #D4D4D4; margin: auto; padding: 10px 20px; border-radius: 10px");
+
+    // var addButton = document.querySelector(".answers");
+    // addButton.appendChild(nextQuestion);
+    // addButton.addEventListener("click", function (){
+    //     console.log("Next Question!");
+    // });
 
 }
 
-
 startQuizEl.addEventListener("click", startQuiz);
-
-// Declare Questions (array)
-// var quizArray = [
-//     {
-//         id: 1,
-//         question: "From the given array which index is the letter 'b' on? ['a', 'b', 'c', 'd']?",
-//         answers: ["0", "1", "2","3"],
-//         correctAnswer: "0"
-//     },
-//     {
-//         id: 2,
-//         question: "What are the two types of scope JavaScript uses?",
-//         answers: ["Inner and Outer", "Main and Peripheral", "Global and Local", "Inclusive and Exclusive"],
-//         correctAnswer: "Global and Local"
-//     }
-// ];
-
 
 //User picks answer
 
